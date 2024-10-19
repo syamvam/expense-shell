39bc06ef779672bb2ff98e49d28bc10b61bad67c
syamkumary <syamyala7@gmail.com>
2024-10-19 21:18:41 +0530

    mysql server creation

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/l.og/expense"
+LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
@@ -30,9 +30,29 @@ VALIDATE(){
     fi
 }
 
-
-
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
+
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
+
+mysql -h mysql.daws81s.online -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting UP root password"
+else
+    echo -e "MySQL root password is already setup...$Y SKIPPING $N" | tee -a $LOG_FILE
+fi
+
+# Assignment
+# check MySQL Server is installed or not, enabled or not, started or not
+# implement the above things
