be05907c54ea2deb70903b55a92109ed2a518233
syamkumary <syamyala7@gmail.com>
2024-10-20 15:29:33 +0530

    backend service

--- mysql.sh
+++ mysql.sh
@@ -43,7 +43,7 @@ VALIDATE $? "Enabled MySQL Server"
 systemctl start mysqld &>>$LOG_FILE
 VALIDATE $? "Started MySQL server"
 
-mysql -h 23.22.177.23 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
+mysql -h 172.31.31.227 -u root -pExpenseApp@1 -e 'show databases;' &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
     echo "MySQL root password is not setup, setting now" &>>$LOG_FILE
